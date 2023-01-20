8d02da9da4746f369e881c5d6dc172043986e779
dinds <dindo.reyes@xiklab.com>
2023-01-19 23:26:27 -0800

    Add project files.

---
+++ arcadian exam/DataLayer/ArcadianDbContext.cs
@@ -0,0 +1,21 @@
+﻿using arcadian_exam.ViewModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace arcadian_exam.DataLayer
+{
+
+    public class ArcadianDbContext : DbContext
+    {
+        public ArcadianDbContext(DbContextOptions
+    <ArcadianDbContext> options) : base(options)
+        {
+        }
+        public DbSet<arcadian> arcadian { get; set; }
+        public DbSet<Country> Country { get; set; }
+
+    }
+}
---
+++ arcadian exam/ViewModel/arcadian.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace arcadian_exam.ViewModel
+{
+    public class arcadian
+    {
+        [Column("DeveloperId")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int DeveloperId { get; set; }
+
+        [Column("DeveloperName")]
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Column("Skills")]
+        [Required]
+        [StringLength(500)]
+        public string Skills { get; set; }
+
+        [ForeignKey("CountryID")]
+        public virtual Country Country { get; set; }
+    }
+}
